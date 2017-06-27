a8c130a37e2e47d69791d64d01b3f6785698f72b
Vinícius Gomes <vgdsouza@live.com>
2017-06-27 09:05:20 -0300

    Tentando fazer login e cadastro

--- app/controllers/UsuarioController.scala
+++ app/controllers/UsuarioController.scala
@@ -1,10 +1,59 @@
 package controllers
 
 import play.api.mvc._
+import models.Usuario
+import models.UsuarioDAO
+import javax.inject.Inject
+import play.api.data._
+import play.api.data.Forms._
+import javax.inject.Singleton
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
 
-class UsuarioController extends Controller {
-  
-  def login(email: String, senha:String) = Action {
-    Ok("foo")
+@Singleton
+class UsuarioController @Inject()(dao: UsuarioDAO, val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val usuarioForm = Form(
+    mapping(
+      "Email" -> nonEmptyText,
+      "Senha" -> nonEmptyText
+    )(UsuarioVO.apply)(UsuarioVO.unapply)
+  )
+
+  def novoUsuario = Action {
+    Ok(views.html.login.sigin(usuarioForm))
+  }
+
+  def novoUsuarioSubmissao = Action { implicit request =>
+    usuarioForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.login.novoUsuario())
+      },
+      usuario => {
+        val novoUsuario = Usuario(0, usuario.email, usuario.senha)
+        dao.salvar(novoUsuario)
+        Created(views.html.login.novoUsuario())
+      }
+    )
+  }
+
+  def login = Action { implicit request =>
+    usuarioForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.login.novoUsuario())
+      },
+      usuario => {
+        val logUsuario = Usuario(0, usuario.email, usuario.senha)
+        var usr = dao.listar
+
+        for (u <- usr) {
+          if (u.email == usuario.email && u.senha == usuario.senha)
+            Ok(views.html.index(true))
+        }
+        Ok(views.html.index(false))
+      }
+    )
   }
 }
+
+case class UsuarioVO(email: String, senha: String)
--- app/models/Usuario.scala
+++ app/models/Usuario.scala
@@ -1,12 +1,31 @@
 package models
 
-case class Usuario(email: String, senha: String)
+import anorm.SQL
+import anorm.SqlQuery
+import anorm.RowParser
+import anorm.Macro
+import anorm.SqlStringInterpolation
+import anorm.SqlParser
+import play.api.db.DB
+import play.api.Play.current
+import javax.inject.Inject
+import play.api.db.Database
+import javax.inject.Singleton
 
-object Usuario {
-  var products = Set(
-        Usuario("foo", "foo"), 
-        Usuario("blah", "blah")
-      )
-      
-   def pesquisaPorEmail(email : String) = products.filter(u => u.email == email).toList(0)  
+case class Usuario(id: Int, email: String, senha: String)
+
+class UsuarioDAO @Inject() (database: Database) {
+  val parser : RowParser[models.Usuario] = Macro.namedParser[models.Usuario]
+
+  def salvar (usuario: Usuario) = database.withConnection { implicit connection =>
+    val id: Option[Long] = SQL(
+      """INSERT INTO TB_USUARIO(EMAIL, SENHA)
+            values ({email}, {senha})""")
+      .on('email -> usuario.email,
+          'senha -> usuario.senha).executeInsert()
+  }
+
+  def listar = database.withConnection { implicit connection =>
+    SQL"SELECT ID, TITULO, DIRETOR, ANO_PRODUCAO AS anoProducao FROM TB_FILME".as(parser.*)
+  }
 }
